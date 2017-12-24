14948f089c3c3d46552d78aa99cdd46377a9ee87
swdev.smz <swdev.smz@gmail.com>
2017-12-24 17:29:38 +0900

    a

--- ExceptionSmaple/Form1.vb
+++ ExceptionSmaple/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        Dim logic As New TestLogic()
+
+        Try
+            logic.run()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
 End Class
