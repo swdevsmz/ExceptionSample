c68d13edac0a258c36cec5af00d8984ce34f15dc
swdev.smz <swdev.smz@gmail.com>
2017-12-25 21:07:11 +0900

    aaa

--- ExceptionSmaple/Form1.vb
+++ ExceptionSmaple/Form1.vb
@@ -4,6 +4,16 @@
         Dim logic As New TestLogic()
 
         Try
+
+            'MessageBox.Show(StatusType.未申請)
+
+            For Each value As StatusType In [Enum].GetValues(GetType(StatusType))
+                Dim intValue As Integer = value
+                Dim strValue As String = value.ToString()
+                Console.WriteLine(strValue & ", " & intValue)
+            Next
+
+
             logic.run()
 
         Catch ex As Exception
