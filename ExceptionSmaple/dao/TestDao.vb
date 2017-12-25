4367d24a9671b1d0510c02d398d5c9e24c7dd409
swdev.smz <swdev.smz@gmail.com>
2017-12-25 21:13:22 +0900

    vvv

---
+++ ExceptionSmaple/dao/TestDao.vb
@@ -0,0 +1,16 @@
+﻿Public Class TestDao
+
+    Public Sub excute()
+
+        Try
+            Dim i(1) As Integer
+            i(3) = 1
+
+
+        Catch ex As Exception
+            Throw New Exception("Value1が9を超えました。")
+        End Try
+
+    End Sub
+
+End Class
---
+++ ExceptionSmaple/datatype/StatusType.vb
@@ -0,0 +1,4 @@
+﻿Public Enum StatusType
+    未申請 = 1
+    申請済 = 2
+End Enum
